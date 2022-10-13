cf8bd4c7e87a394952ae635b1d46e08383b6879a
yianyian5 <44080940+yianyian5@users.noreply.github.com>
2022-10-13 15:59:24 +0800

    ta

--- TA algorithm/TS.py
+++ TA algorithm/TS.py
@@ -6,4 +6,5 @@ def generate_threshold_sequence(m, n, D_full, n_i, f):
     t = np.arange(2*n_i)
     for i in t:
         index = np.random.choice(np.arange(D_full.shape[0]),n,False)
-        D = D_full[index,:]
+        D = D_full[index]
+        D_1 = generate_neighborhood(D)
